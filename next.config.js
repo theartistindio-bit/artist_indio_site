c67a30e833fb5436d3e749f1feea134398e8d736
theartistindio-bit <theartist.indio@gmail.com>
2025-10-19 18:15:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 const nextConfig = {
   reactStrictMode: true
 };
+
+module.exports = nextConfig;
+** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
 module.exports = nextConfig;
 .exports = { reactStrictMode: true, experimental: { serverActions: false } };
