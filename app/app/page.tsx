fd8b3442d57d501c9c6979d0cfb23787f155e605
theartistindio-bit <theartist.indio@gmail.com>
2025-10-19 19:04:45 -0400

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main style={{minHeight:'100vh',display:'flex',alignItems:'center',justifyContent:'center',background:'#111',color:'#fff'}}>
+      <h1>Welcome to The Artist Indio</h1>
+    </main>
+  );
+}
